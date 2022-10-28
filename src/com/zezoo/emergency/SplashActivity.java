2f443ed12b6753f4c8a8bf55cad9a1b3a14d7b10
Majd Zain AL Deen <zezoocvi.77@gmail.com>
2022-10-28 16:03:24 +0300

    second commit

--- src/com/zezoo/emergency/SplashActivity.java
+++ src/com/zezoo/emergency/SplashActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-//
+//abc
 public class SplashActivity extends Activity {
 	/** Duration of wait **/
     private final int SPLASH_DISPLAY_LENGTH = 6000;
